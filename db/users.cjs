c6ed915b82a6c130376463af3ba9bbd9b90ca726
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-03-13 13:33:44 -0400

    login and auth user funcs in users.cjs

--- db/users.cjs
+++ db/users.cjs
@@ -16,17 +16,48 @@ const createUsers = async (first_name, last_name, password, username, email) => 
   }
 };
 
-const getUserByUsername = async (username) => {
+const authenticateUser = async (username, password) => {
   try {
-    const result = await client.query(`
-      SELECT * FROM users
-      WHERE username = $1;
-      `, [username]);
+    const { rows } = await client.query(`
+      SELECT * FROM users 
+      WHERE username= $1;
+    `, [username]);
 
-      return result.rows[0];
-  }catch(err){
-    console.log(err);
+    const user = rows[0];
+    if (user) {
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (isPasswordMatch) {
+        const token = await jwt.sign({ username: user.username }, process.env.JWT_SECRET);
+        return token;
+      } else {
+        throw new Error('No User Found');
+      }
+    } else {
+      throw new Error('No User Found');
+    }
+  } catch (err) {
+    throw new Error('No User Found');
+  }
+}
+
+const logInWithToken = async (token) => {
+  try {
+    const usefulInfo = await jwt.verify(token, process.env.JWT_SECRET);
+
+    const { rows } = await client.query(`
+      SELECT * FROM users WHERE username='${usefulInfo.username}'
+    `);
+
+    const user = rows[0];
+
+    if (user) {
+      return { username: user.username };
+    } else {
+      return { message: 'Bad Token' };
+    }
+  } catch (err) {
     throw err;
   }
 }
-module.exports = { createUsers, getUserByUsername };
+
+module.exports = { createUsers, authenticateUser, logInWithToken };
